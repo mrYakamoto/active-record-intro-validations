1b516b1543885f7977f2b93aa727b600d6cc02e0
GregoryW <email": null>
2015-12-17 16:15:23 -0800

    finish validations intro

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,4 +1,6 @@
 class Person < ActiveRecord::Base
   has_many :dogs, { foreign_key: :owner_id }
   has_many :ratings, { foreign_key: :judge_id }
+
+  validates :first_name, :last_name, { :presence => true }
 end
